d066973cce73329450ae837220e69685835b9890
Gustavo <paez.gustavo@outlook.com>
2019-05-23 09:45:40 -0500

    Add ReadValuesFromTerminal statements

--- ReadFile.py
+++ ReadFile.py
@@ -7,8 +7,13 @@
     #if firstLine in open("test/test.txt").read():
     #    print(firstLine + "--> exist")
 
+print("File name (and route if its necessary) to comparate (add the file type too - test.txt) ")
+s = input()
+print("File name 2 to comparate (add route if its necessary)")
+r = input()
+print("input1 : " + s + "/ input2 :" + r)
     #Example 1
-fileHandler = open ("test.txt", "r")
+fileHandler = open (s, "r") #fileHandler = open ("test.txt", "r")
     # Get list of all lines in file
 listOfLines = fileHandler.readlines()
     # Close file
@@ -17,7 +22,7 @@ fileHandler.close()
     # Iterate over the lines
 for line in listOfLines:
     #print(line.strip())
-    if line.strip() in open("test/test.txt").read():
+    if line.strip() in open(r).read(): #if line.strip() in open("test/test.txt").read():
         print(line.strip() + " ---> exits")
     else:
         print(line.strip() + " ---> not exist")
